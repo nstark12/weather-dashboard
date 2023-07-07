591da1432ea9ec6400240e56f341796660b305c5
Marketing Director <nicolestark1103@gmail.com>
2023-07-07 11:04:49 -0500

    started getting 5 day data to work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 var apiKey = "e6676e78c5006f290530288e910ece07";
 var currentWeather = document.querySelector(".current-weather");
+var extendedWeather = document.querySelector(".extended-weather");
 var cityInputEl = document.querySelector(".search-city");
 var cityForm = document.querySelector("#city-form")
+var searchHistory = [];
 
 function getCityInput(event) {
     event.preventDefault();
     var cityName = cityInputEl.value;
+    clearCurrent();
     getWeatherByCity(cityName);
+    get5DayForecast(cityName);
 }
 
 function getWeatherByCity(cityName) {
@@ -20,20 +24,21 @@ function getWeatherByCity(cityName) {
             console.log(weatherData)
             // create a title with city name
             var h2 = document.createElement('h2');
-            h2.innerText = cityName;
+            // capitalize first letter of city name
+            h2.innerText = cityName.charAt(0).toUpperCase() + cityName.slice(1);
             // append new title to currentWeather div
             currentWeather.appendChild(h2);
             // create list items
                 var ul = document.createElement('ul');
                 // temperature
                 var li1 = document.createElement('li');
-                li1.innerText = "Temp: " + weatherData.main.temp;
+                li1.innerText = "Temp: " + weatherData.main.temp + " °F";
                 // wind
                 var li2 = document.createElement('li');
-                li2.innerText = "Wind: " + weatherData.wind.speed;
+                li2.innerText = "Wind: " + weatherData.wind.speed + " mph";
                 // humidity
                 var li3 = document.createElement('li');
-                li3.innerText = "Humidity: " + weatherData.main.humidity;
+                li3.innerText = "Humidity: " + weatherData.main.humidity + " %";
                 // append list items to unordered list
                 ul.appendChild(li1);
                 ul.appendChild(li2);
@@ -50,6 +55,36 @@ function get5DayForecast(cityName) {
             console.log(response)
             return response.json()
         })
+        .then(function(fiveDayData) {
+            console.log(fiveDayData)
+            // create a loop to make a weather card for each day
+
+            var forecast = fiveDayData.list;
+            for (var i = 0; i < 5; i++) {
+                var dailyForecast = forecast[i];
+                // var date = fiveDayData.main[i].dt
+                // console.log(date)
+                var ul2 = document.createElement('ul');
+                var tempEl = document.createElement('li');
+                tempEl.innerText = "Temp: " + dailyForecast.main.temp + " °F";
+                
+                ul2.appendChild(tempEl);
+                extendedWeather.appendChild(ul2);
+                console.log(tempEl)
+
+            }
+
+        })
+}
+
+function clearCurrent() {
+    var currentConditionsEl = document.querySelector(".current-weather");
+    currentConditionsEl.innerText = "";
+
+    var fiveDayConditionsEl = document.querySelector(".extended-weather");
+    fiveDayConditionsEl.innerText = "";
+
+    return;
 }
 
 // event listener for city search
